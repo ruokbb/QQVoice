b8b5be016301182adcb93bf2e2991473ff3be5ac
ruokbb <ruokbb@qq.com>
2024-02-05 14:51:07 +0800

    [fix] slk后缀

--- parser/slk_product.py
+++ parser/slk_product.py
@@ -14,19 +14,18 @@ import os
 
 def get_silk_from_wav(wav_path: str) -> str:
     """尝试将wav转换成silk文件"""
-    silk_path = wav_path.replace('.wav', '.silk')
-    if Path(silk_path).exists():
-        os.remove(silk_path)
+    slk_path = wav_path.replace('.wav', '.slk')
+    if Path(slk_path).exists():
+        os.remove(slk_path)
 
     command = "sh ./bin/wav2silk.sh {}".format(wav_path)
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     stdout, stderr = p.communicate()
     errMsg = stderr.decode().strip()
-    if errMsg: # ffmpeg 把所有信息输出到 stderr 了。。。
-        print(errMsg)
+    # if errMsg: # ffmpeg 把所有信息输出到 stderr 了。。。
+    #     print(errMsg)
 
-    if Path(silk_path).exists():
-        return silk_path
+    if Path(slk_path).exists():
+        return slk_path
     else:
-        print("slk生成失败")
         return ""
