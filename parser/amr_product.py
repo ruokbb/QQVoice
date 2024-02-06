1b8f659ebfc93ae6ddfe09d71c4234b697cccfd8
ruokbb <ruokbb@qq.com>
2024-02-06 17:46:26 +0800

    [update] 微信语音替换

--- parser/amr_product.py
+++ parser/amr_product.py
@@ -14,17 +14,21 @@ import os
 def get_amr_from_wav(wav_path: str) -> str:
     """尝试将wav转换成silk文件"""
     amr_path = wav_path.replace('.wav', '.amr')
+    slk_path = wav_path.replace('.wav', '.slk')
     if Path(amr_path).exists():
         os.remove(amr_path)
+    if Path(slk_path).exists():
+        os.remove(slk_path)
 
-    command = "sh ./bin/wav2amr.sh {}".format(wav_path)
+    command = "sh ./bin/wav2slk.sh {}".format(wav_path)
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     stdout, stderr = p.communicate()
     errMsg = stderr.decode().strip()
     # if errMsg: # ffmpeg 把所有信息输出到 stderr 了。。。
     #     print(errMsg)
 
-    if Path(amr_path).exists():
+    if Path(slk_path).exists():
+        os.rename(slk_path, amr_path)
         return amr_path
     else:
         return ""
